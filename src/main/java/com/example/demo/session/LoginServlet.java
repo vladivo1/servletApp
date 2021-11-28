46d5e0e51c645f9e2a3fe8105dd70b69e80f592e
vladivo1 <vla.ivolga@gmail.com>
2021-11-28 18:28:37 +0200

    save log_and_pass_in_map

--- src/main/java/com/example/demo/session/LoginServlet.java
+++ src/main/java/com/example/demo/session/LoginServlet.java
@@ -5,6 +5,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Servlet implementation class LoginServlet
@@ -16,26 +18,30 @@ public class LoginServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Это название 2-х параметров, которые мы передаем
-        String user = request.getParameter("user");
+        String log = request.getParameter("user");
         String pwd = request.getParameter("pwd");
-        // Это значение наших параметров
-        String userID = "admin";
-        String password = "password";
 
-        if (userID.equals(user) && password.equals(pwd)) {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", "user");
-            //setting session to expiry in 30 mins
-            session.setMaxInactiveInterval(30 * 60);
-            Cookie userName = new Cookie("user", user);
-            userName.setMaxAge(30 * 60);
-            response.addCookie(userName);
-            PrintWriter out = response.getWriter();
-            out.println("Welcome back to the team, " + user + "!");
-        } else {
-            PrintWriter out = response.getWriter();
-            out.println("Either user name or password is wrong!");
+        Map<String,String> usersMap = new HashMap<>();
+        usersMap.put(log,pwd);
+
+        for (Map.Entry<String,String> kv: usersMap.entrySet()) {
+           if (kv.getValue().equals(pwd)) {
+                HttpSession session = request.getSession();
+                session.setAttribute("user", "user");
+                //setting session to expiry in 30 min
+                session.setMaxInactiveInterval(30 * 60);
+                Cookie userName = new Cookie("user", log);
+                userName.setMaxAge(30 * 60);
+                response.addCookie(userName);
+                PrintWriter out = response.getWriter();
+                out.println("Welcome back to the team, " + log + "!");
+            } else {
+                PrintWriter out = response.getWriter();
+                out.println("Either user name or password is wrong!");
+            }
         }
+            }
     }
-}
+
+
+
