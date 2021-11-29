123b9e77edb57d3ef1843f79d112f47de78ec1ce
vladivo1 <vla.ivolga@gmail.com>
2021-11-29 15:37:42 +0200

    save log_and_pass_in_Map2.0

--- src/main/java/com/example/demo/session/LoginServlet.java
+++ src/main/java/com/example/demo/session/LoginServlet.java
@@ -21,11 +21,12 @@ public class LoginServlet extends HttpServlet {
         String log = request.getParameter("user");
         String pwd = request.getParameter("pwd");
 
-        Map<String,String> usersMap = new HashMap<>();
-        usersMap.put(log,pwd);
+        Map<String, String> usersMap = new HashMap<>();
+        usersMap.put("admin", "password");
+        usersMap.put("vlad", "vlad");
 
-        for (Map.Entry<String,String> kv: usersMap.entrySet()) {
-           if (kv.getValue().equals(pwd)) {
+        for (Map.Entry<String, String> kv : usersMap.entrySet()) {
+            if (kv.getKey().equals(log) && kv.getValue().equals(pwd)) {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", "user");
                 //setting session to expiry in 30 min
@@ -34,14 +35,14 @@ public class LoginServlet extends HttpServlet {
                 userName.setMaxAge(30 * 60);
                 response.addCookie(userName);
                 PrintWriter out = response.getWriter();
-                out.println("Welcome back to the team, " + log + "!" + "You using Map for save your login and password!");
+                out.println("Welcome back to the team, " + log + "!" + " You using Map for save your login and password!");
             } else {
                 PrintWriter out = response.getWriter();
                 out.println("Either user name or password is wrong!");
             }
         }
-            }
     }
+}
 
 
 
